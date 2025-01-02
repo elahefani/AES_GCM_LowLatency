b3701d808898d111845e8067d4d669afa9586e39
YouS0 <younes.creed.box@gmail.com>
2025-01-02 15:19:54 +0330

    add clock and register to the GF(2^128)

--- src/gf_tb.v
+++ src/gf_tb.v
@@ -1,29 +1,45 @@
-`timescale 1ns / 1ps
-
+`timescale 1ps/1ps
 module gfmul_tb;
+  // Testbench signals
+  reg clk;
+  reg [127:0] iCtext;
+  reg [127:0] iHashkey;
+  wire [127:0] oResult;
 
-// Declare inputs as regs and outputs as wires
-reg [0:127] iCtext;
-reg [0:127] iHashkey;
-wire [0:127] oResult;
-
-// Instantiate the Unit Under Test (UUT)
-gfmul uut (
+  // Instantiate the gfmul module
+  gfmul uut (
+    .clk(clk),
     .iCtext(iCtext),
     .iHashkey(iHashkey),
     .oResult(oResult)
-);
+  );
 
-initial begin
-    // Test vectors
-    // Apply the first test case
-    iCtext = 128'hfeedfacedeadbeeffeedfacedeadbeef;
-    iHashkey = 128'h466923ec9ae682214f2c082badb39249;
-    #100; // Wait for 10 ns
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; // 100 MHz clock
+  end
 
-    // Display the result
-    $display("Test 1: iCtext = %h, iHashkey = %h, oResult = %h", iCtext, iHashkey, oResult);
-    $stop;
-end
+  // Test vector generation
+  initial begin
+    // Monitor output
+    // Initialize inputs
+    iCtext = 128'h0;
+    iHashkey = 128'h0;
 
+    // Apply test vectors
+    #10     iCtext = 128'hfeedfacedeadbeeffeedfacedeadbeef;
+            iHashkey = 128'hb83b533708bf535d0aa6e52980d53b78;
+
+    #100 $display(" clk=%b, iCtext=%h, iHashkey=%h, oResult=%h", clk, iCtext, iHashkey, oResult);
+    #10 iCtext = 128'hffffffffffffffffffffffffffffffff;
+        iHashkey = 128'h00000000000000000000000000000001;
+    #100 $display(" clk=%b, iCtext=%h, iHashkey=%h, oResult=%h", clk, iCtext, iHashkey, oResult);
+
+    #10 iCtext = 128'h123456789abcdef0123456789abcdef0;
+        iHashkey = 128'h11111111111111111111111111111111;
+    #100 $display(" clk=%b, iCtext=%h, iHashkey=%h, oResult=%h", clk, iCtext, iHashkey, oResult);
+
+    #20 $stop; // End simulation
+  end
 endmodule
